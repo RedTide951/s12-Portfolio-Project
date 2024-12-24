607d0a7a63f319442da4b2baadf4045e82fce544
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2024-12-25 01:42:11 +0300

    appbar added

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,8 +1,26 @@
+// eslint-disable-next-line no-unused-vars
 import React, { Component } from "react";
+import "./appbar.css";
 
 export class AppBar extends Component {
   render() {
-    return <div>AppBar</div>;
+    return (
+      <div className="app-bar">
+        <div className="left-divison"></div>
+        <div className="right-divison">
+          <input
+            type="checkbox"
+            value="synthwave"
+            className="toggle theme-controller theme-button"
+          />
+          <p>DARK MODE</p>
+          <div>|</div>
+          <p>
+            <span className="accenture">TÜRKÇE</span>'YE GEÇ
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
