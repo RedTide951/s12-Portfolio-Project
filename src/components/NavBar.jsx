1d68c5a6975a9cc6aae184ac30fd69aed36dca5b
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2024-12-25 02:02:27 +0300

    appbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import "./navbar.css";
 
 export class NavBar extends Component {
   render() {
-    return <div>NavBar</div>;
+    return (
+      <div className="nav-bar">
+        <div className="left-divison"></div>
+        <div className="right-divison">
+          <button className="btn btn-outline nav-link">Skills</button>
+          <button className="btn btn-outline nav-link">Projects</button>
+          <button className="btn btn-outline nav-button">Hire me</button>
+        </div>
+      </div>
+    );
   }
 }
 
