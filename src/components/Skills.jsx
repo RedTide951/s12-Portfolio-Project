888d686879407129d9b0f24b73f05086739da2d5
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2024-12-26 23:03:14 +0300

    skills section added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
+import "./skills.css";
 
 export class Skills extends Component {
   render() {
-    return <div>Skills</div>;
+    return (
+      <div className="skills-container">
+        <h2 className="heading">Skills</h2>
+        <div className="skills-row">
+          <div className="card">
+            <h3 className="subheading">JavaScript</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              magnam officiis commodi, ipsa, sequi rem consectetur veritatis a
+              expedita autem doloremque? Incidunt delectus quos eligendi debitis
+              eaque aut. Ad, repellat.
+            </p>
+          </div>
+          <div className="card">
+            <h3 className="subheading">React</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              magnam officiis commodi, ipsa, sequi rem consectetur veritatis a
+              expedita autem doloremque? Incidunt delectus quos eligendi debitis
+              eaque aut. Ad, repellat.
+            </p>
+          </div>
+          <div className="card">
+            <h3 className="subheading">Node.js</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              magnam officiis commodi, ipsa, sequi rem consectetur veritatis a
+              expedita autem doloremque? Incidunt delectus quos eligendi debitis
+              eaque aut. Ad, repellat.
+            </p>
+          </div>
+        </div>
+        <div className="divider"></div>
+      </div>
+    );
   }
 }
 
