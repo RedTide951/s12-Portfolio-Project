56a63e8fec4dc602f03a81dcd3a474eb54a16e86
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2024-12-28 01:00:47 +0300

    github api added for mapping project cards

---
+++ src/api/githubAPI.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const username = "RedTide951";
+const repoNames = ["s8-challenge-pizza", "witflix", "RT-Snake"];
+
+let repos = {};
+
+async function fetchSelectedRepos() {
+  try {
+    const requests = repoNames.map((repoName) =>
+      axios.get(`https://api.github.com/repos/${username}/${repoName}`)
+    );
+
+    const responses = await Promise.all(requests);
+    repos = responses.map((response) => ({
+      name: response.data.name,
+      description: response.data.description,
+      gitUrl: response.data.git_url,
+      homepage: response.data.homepage,
+    }));
+    console.log(repos);
+  } catch (error) {
+    console.error("Error fetching repositories:", error);
+  }
+}
+
+fetchSelectedRepos();
