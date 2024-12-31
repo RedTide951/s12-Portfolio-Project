1e9f84139dc32c4d8789b440f9baea4cecb67633
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2024-12-31 15:10:52 +0300

    project images added/fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, require } from "react";
 import "./projects.css";
 import { LanguageContext } from "../contexts/LanguageContext";
 // import fetchSelectedRepos from "../api/githubAPI";
@@ -6,7 +6,7 @@ import { LanguageContext } from "../contexts/LanguageContext";
 export const Projects = () => {
   const { text } = useContext(LanguageContext);
 
-  // Dont use API for now
+  // Dont use github API for now
   /* const [projectData, setProjectData] = useState([]); */
   /* useEffect(() => {
     const loadProjects = async () => {
@@ -21,43 +21,51 @@ export const Projects = () => {
     <div className="wrapper">
       <h2 className="heading">{text.projects}</h2>
       <div className="projects-container py-4 pb-8 flex gap-6 flex-col sm:flex-row sm:overflow-x-auto sm:flex-nowrap sm:-mx-8 sm:px-6 sm:gap-6 sm:mb-8 lg:grid lg:grid-cols-3 lg:gap-12  lx:pb-12 ">
-        {text.projectsData.map((project, index) => (
-          <div
-            className="card card-compact card-effects bg-base-100 shadow-lg max-w-fit sm:max-w-72 sm:flex-shrink-0 lg:max-w-fit"
-            key={index}
-          >
-            <figure>
-              <img className="" src={project.image} alt={project.name} />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-primary">{project.name}</h2>
-              <p className="text-base">{project.description}</p>
-              <div className="flex gap-2">
-                {project.keywords.map((keyword, keywordIndex) => (
-                  <div className="badge badge-outline" key={keywordIndex}>
-                    {keyword}
-                  </div>
-                ))}
-              </div>
-              <div className="card-actions flex gap-2 justify-end content-center">
-                <a
-                  className="btn btn-sm hover:text-primary"
-                  target="_blank"
-                  href={project.gitUrl}
-                >
-                  Github
-                </a>
-                <a
-                  className="btn btn-sm hover:text-primary"
-                  target="_blank"
-                  href={project.homepage}
-                >
-                  View site
-                </a>
+        {text.projectsData.map((project, index) => {
+          console.log("bura", project.imageUrl);
+          console.log("imgname", project.imageName);
+          return (
+            <div
+              className="card card-compact card-effects bg-base-100 shadow-lg max-w-fit sm:max-w-72 sm:flex-shrink-0 lg:max-w-fit"
+              key={index}
+            >
+              <figure>
+                <img
+                  className=""
+                  src={`${project.imageUrl}`}
+                  alt={project.name}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-primary">{project.name}</h2>
+                <p className="text-base">{project.description}</p>
+                <div className="flex gap-2">
+                  {project.keywords.map((keyword, keywordIndex) => (
+                    <div className="badge badge-outline" key={keywordIndex}>
+                      {keyword}
+                    </div>
+                  ))}
+                </div>
+                <div className="card-actions flex gap-2 justify-end content-center">
+                  <a
+                    className="btn btn-sm hover:text-primary"
+                    target="_blank"
+                    href={project.gitUrl}
+                  >
+                    Github
+                  </a>
+                  <a
+                    className="btn btn-sm hover:text-primary"
+                    target="_blank"
+                    href={project.homepage}
+                  >
+                    View site
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
