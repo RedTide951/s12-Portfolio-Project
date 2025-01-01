02574970dee15828a71ed5f8d7aa9f9922df9c4d
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2025-01-01 13:01:36 +0300

    localstorage now working but failing to read when page refreshes

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -4,8 +4,13 @@ export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = localStorage.getItem(key);
-      console.log(`Retrieved from localStorage: ${key} = ${item}`);
-      return item ? JSON.parse(item) : initialValue;
+
+      if (item) {
+        return JSON.parse(item);
+      } else {
+        localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
+      }
     } catch (error) {
       console.error("Error reading from localStorage", error);
       return initialValue;
@@ -13,13 +18,16 @@ export const useLocalStorage = (key, initialValue) => {
   });
 
   const setValue = (value) => {
-    try {
-      console.log("Saving to localStorage:", key, value);
-      setStoredValue(value);
-      localStorage.setItem(key, JSON.stringify(value));
-    } catch (error) {
-      console.error("Error writing to localStorage", error);
+    let newValue = value;
+
+    if (typeof value === "function") {
+      const fn = value;
+      newValue = fn(storedValue);
+    } else {
+      newValue = value;
     }
+    localStorage.setItem(key, JSON.stringify(newValue));
+    setStoredValue(newValue);
   };
 
   return [storedValue, setValue];
