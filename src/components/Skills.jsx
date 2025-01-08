946796a176d5a4e35b03ea48a1dd0a5e57215eba
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2025-01-08 04:49:35 +0300

    improvements

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,31 +13,61 @@ export const Skills = () => {
         <div className="mockup-browser-toolbar">
           <div className="input">{text.mockupWebsiteUrl}</div>
         </div>
-        <div className="bg-base-200 flex flex-col justify-center px-10 py-16 gap-8">
+        <div className="bg-base-200 flex flex-col justify-center px-14 py-10 gap-6">
           <div className="skills-row flex-col md:flex-row">
-            <div className="flex-1 bg-base-300 p-4 rounded-lg">
+            <div className="flex-1 bg-base-300 p-4 rounded-lg max-h-96 overflow-y-auto">
+              <p>
+                <h3 className="text-primary subheading">JavaScript</h3>{" "}
+              </p>
+              <p>{text.skillDescriptions.js}</p>
+              <br />
+              <p>
+                <h3 className="text-primary subheading">React.JS</h3>{" "}
+              </p>
+              <p>{text.skillDescriptions.react}</p>
+              <br />
+              <p>
+                <h3 className="text-primary subheading">Java</h3>{" "}
+              </p>
+              <p>{text.skillDescriptions.java}</p>
+            </div>
+            <div className="flex-1 bg-base-200 p-4 rounded-lg">
+              <p>
+                <h3 className="text-primary subheading">{text.coreSkills}</h3>{" "}
+              </p>
+              <ul className="list-disc list-inside ml-4">
+                {text.skillsList.core.map((proficiency, index) => (
+                  <li key={index} className="text-lg">
+                    {proficiency}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex-1 bg-base-200 p-4 rounded-lg">
               <p>
                 <h3 className="text-primary subheading">
                   {text.proficiencies}
                 </h3>{" "}
               </p>
-              <ul className="list-disc list-inside ml-4">
-                {text.skillsList.core.map((proficiency, index) => (
-                  <li key={index}>{proficiency}</li>
+              <ul className="list-disc list-inside ml-4 ">
+                {text.skillsList.proficiencies.map((proficiency, index) => (
+                  <li key={index} className="text-lg">
+                    {proficiency}
+                  </li>
                 ))}
               </ul>
             </div>
-            <div className="flex-1">
-              <h3 className="subheading text-primary">JavaScript</h3>
-              <p>{text.skillDescriptions.js}</p>
-            </div>
-            <div className="flex-1">
-              <h3 className="subheading text-primary">React</h3>
-              <p>{text.skillDescriptions.react}</p>
-            </div>
-            <div className="flex-1">
-              <h3 className="subheading text-primary">Java</h3>
-              <p>{text.skillDescriptions.java}</p>
+            <div className="flex-1 bg-base-200 p-4 rounded-lg">
+              <p>
+                <h3 className="text-primary subheading">{text.additional}</h3>{" "}
+              </p>
+              <ul className="list-disc list-inside ml-4">
+                {text.skillsList.additional.map((proficiency, index) => (
+                  <li key={index} className="text-lg">
+                    {proficiency}
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
